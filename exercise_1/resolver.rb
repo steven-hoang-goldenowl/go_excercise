7a2034f6b05de530a6017c4132da79c58fbeebac
steven <steven.hoang.goldenowl@gmail.com>
2022-06-17 16:48:12 +0700

    steven excercise 1

--- exercise_1/resolver.rb
+++ exercise_1/resolver.rb
@@ -1,1 +1,34 @@
-# write code here...
+class Calculate
+  def initialize(sum, arr = [])
+    @sum = sum
+    @arr = arr
+  end
+
+  def calculate_desired_sum
+    results = []
+    except_numbers = []
+
+    @arr.each do |number|
+      next if number > @sum
+
+      results << [number, @sum - number] if valid_number?(except_numbers, number)
+      except_numbers << number
+    end
+
+    results
+  end
+
+  private
+
+  def valid_number?(except_numbers, first_number)
+    second_number = @sum - first_number
+    return false if first_number == second_number
+    return false unless @arr.include?(second_number)
+    return false if except_numbers.include?(first_number) || except_numbers.include?(second_number)
+
+    true if @arr.include?(second_number)
+  end
+end
+
+# p Calculate.new(10, [3, 4, 5, 6, 7]).calculate_desired_sum
+# p Calculate.new(8, [3, 4, 5, 4, 4]).calculate_desired_sum
--- exercise_1/resolver_test.rb
+++ exercise_1/resolver_test.rb
@@ -1,1 +1,21 @@
-# write code here...
+require 'minitest/autorun'
+require './resolver'
+class TestCalculate < Minitest::Test
+  describe 'when input is (10, [3, 4, 5, 6, 7])' do
+    before do
+      @data = Calculate.new(10, [3, 4, 5, 6, 7])
+    end
+    it 'must equal [[4, 6], [3, 7]]' do
+      @data.calculate_desired_sum.must_equal [[3, 7], [4, 6]]
+    end
+  end
+
+  describe 'when input is (8, [3, 4, 5, 4, 4])' do
+    before do
+      @data = Calculate.new(8, [3, 4, 5, 4, 4])
+    end
+    it 'must equal [(3, 5)]' do
+      @data.calculate_desired_sum.must_equal [[3, 5]]
+    end
+  end
+end
